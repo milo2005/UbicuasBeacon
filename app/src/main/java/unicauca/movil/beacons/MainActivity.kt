95429404bc297deb61c55a1cddb932242f315015
Dario Chamorro <dario.chamorro.v@gmail.com>
2017-11-27 09:16:27 -0500

    Bluetooth permission implemented

--- app/src/main/java/unicauca/movil/beacons/MainActivity.kt
+++ app/src/main/java/unicauca/movil/beacons/MainActivity.kt
@@ -3,6 +3,7 @@ package unicauca.movil.beacons
 import android.content.IntentFilter
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.estimote.coresdk.common.requirements.SystemRequirementsChecker
 import unicauca.movil.beacons.receivers.BeaconReceiver
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        SystemRequirementsChecker.checkWithDefaultDialogs(this)
     }
 
     override fun onResume() {
